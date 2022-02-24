8885d4504f767338f883d54fca1097d0471cf94e
JoelBCarter <joelbcarter@gmail.com>
2022-02-23 20:51:15 -0600

    Use default query

--- src/location/query/post/post.spec.ts
+++ src/location/query/post/post.spec.ts
@@ -1,15 +1,18 @@
 import { StatusCodes } from 'http-status-codes'
 
-import { createQuery } from '../../../test'
-import { LocationDivinerQueryRequest } from './postLocationQuerySchema'
+import { createQuery, getValidRequest } from '../../../test'
 
 describe('/location/query', () => {
   describe('Archive Config', () => {
     it('With invalid source archive config', async () => {
-      await createQuery({} as LocationDivinerQueryRequest, StatusCodes.BAD_REQUEST)
+      const query = getValidRequest()
+      query.sourceArchive.apiDomain = ''
+      await createQuery(query, StatusCodes.BAD_REQUEST)
     })
     it('With invalid response archive config', async () => {
-      await createQuery({} as LocationDivinerQueryRequest, StatusCodes.BAD_REQUEST)
+      const query = getValidRequest()
+      query.resultArchive.apiDomain = ''
+      await createQuery(query, StatusCodes.BAD_REQUEST)
     })
   })
 })
