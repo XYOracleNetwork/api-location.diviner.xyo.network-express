42d0a2a1a61adb9a4119a9fddb86e407199ce680
JoelBCarter <joelbcarter@gmail.com>
2022-02-23 20:37:24 -0600

    refactor error messages

--- src/location/query/post/post.ts
+++ src/location/query/post/post.ts
@@ -5,26 +5,26 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { LocationDivinerQueryRequest } from './postLocationQuerySchema'
 import { validateArchiveConfig } from './validateArchiveConfig'
 
+const sourceArchiveConfigError = {
+  message: 'Invalid source archive config',
+  name: ReasonPhrases.BAD_REQUEST,
+  statusCode: StatusCodes.BAD_REQUEST,
+}
+
+const resultArchiveConfigError = {
+  message: 'Invalid result archive config',
+  name: ReasonPhrases.BAD_REQUEST,
+  statusCode: StatusCodes.BAD_REQUEST,
+}
+
 const handler: RequestHandler<NoReqParams, LocationDivinerQueryRequest, LocationDivinerQueryRequest> = (
   req,
   res,
   next
 ) => {
   const { sourceArchive, resultArchive, query } = req.body
-  if (!validateArchiveConfig(sourceArchive)) {
-    next({
-      message: 'Invalid source archive config',
-      name: ReasonPhrases.BAD_REQUEST,
-      statusCode: StatusCodes.BAD_REQUEST,
-    })
-  }
-  if (!validateArchiveConfig(resultArchive)) {
-    next({
-      message: 'Invalid result archive config',
-      name: ReasonPhrases.BAD_REQUEST,
-      statusCode: StatusCodes.BAD_REQUEST,
-    })
-  }
+  if (!validateArchiveConfig(sourceArchive)) next(sourceArchiveConfigError)
+  if (!validateArchiveConfig(resultArchive)) next(resultArchiveConfigError)
   next({ message: ReasonPhrases.NOT_IMPLEMENTED, statusCode: StatusCodes.NOT_IMPLEMENTED })
 }
 
