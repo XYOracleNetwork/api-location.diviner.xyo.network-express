6a2656295ec2abc688dacec59854a6bf7bb18eca
JoelBCarter <joelbcarter@gmail.com>
2022-02-24 01:06:27 -0600

    Tests to get query result

--- src/location/query/query/get/get.spec.ts
+++ src/location/query/query/get/get.spec.ts
@@ -1,15 +1,21 @@
 import { createQuery, getQuery } from '../../../../test'
 import { LocationDivinerQueryCreationResponse } from '../../post'
 
+// TODO: Remove hardcoded delay as they always result in intermittent
+// test failures
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
 describe('GET /location/query/:hash', () => {
   let queryResponse: LocationDivinerQueryCreationResponse
   beforeEach(async () => {
     queryResponse = await createQuery()
   })
   it('Gets successful results', async () => {
+    // Wait for query to process
+    await delay(5000)
     const result = await getQuery(queryResponse.hash)
     expect(result).toBeTruthy()
     expect(result.queryHash).toBe(queryResponse.hash)
     expect(result.answerHash).toBeTruthy()
-  })
+  }, 10000)
 })
