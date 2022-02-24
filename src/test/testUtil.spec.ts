4f065df6af77435e2bc1d8895c5864ddf1ae2694
JoelBCarter <joelbcarter@gmail.com>
2022-02-23 20:48:37 -0600

    Setup default ENV VARs to point to localhost

--- src/test/testUtil.spec.ts
+++ src/test/testUtil.spec.ts
@@ -3,14 +3,22 @@ import supertest, { SuperTest, Test } from 'supertest'
 
 import { LocationDivinerQueryRequest, LocationDivinerQueryResult } from '../location'
 
-test.skip('Must have API_KEY ENV VAR defined', () => {
-  expect(process.env.API_KEY).toBeTruthy()
+test('Must have ARCHIVIST_URL ENV VAR defined', () => {
+  expect(process.env.ARCHIVIST_URL).toBeTruthy()
+})
+
+test('Must have ARCHIVE ENV VAR defined', () => {
+  expect(process.env.ARCHIVE).toBeTruthy()
 })
 
 test('Must have APP_PORT ENV VAR defined', () => {
   expect(process.env.APP_PORT).toBeTruthy()
 })
 
+const startTime = new Date(0).toISOString()
+const apiDomain = process.env.ARCHIVIST_URL || 'http://localhost:8080'
+const archive = process.env.ARCHIVE || 'temp'
+const schema = 'location.diviner.xyo.network'
 const request = supertest(`http://localhost:${process.env.APP_PORT}`)
 
 export const getDiviner = (): SuperTest<Test> => {
@@ -18,8 +26,12 @@ export const getDiviner = (): SuperTest<Test> => {
 }
 
 export const getValidRequest = (): LocationDivinerQueryRequest => {
-  // TODO: Get request
-  return {} as LocationDivinerQueryRequest
+  const stopTime = new Date().toISOString()
+  return {
+    query: { schema, startTime, stopTime },
+    resultArchive: { apiDomain, archive },
+    sourceArchive: { apiDomain, archive },
+  }
 }
 
 export const createQuery = async (
