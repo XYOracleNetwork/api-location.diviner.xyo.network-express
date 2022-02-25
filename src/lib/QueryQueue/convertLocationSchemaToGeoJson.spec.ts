96a854568647a469e0395627e8fc3734d5bb6578
JoelBCarter <joelbcarter@gmail.com>
2022-02-25 00:43:02 -0600

    Update tests

--- src/lib/QueryQueue/convertLocationSchemaToGeoJson.spec.ts
+++ src/lib/QueryQueue/convertLocationSchemaToGeoJson.spec.ts
@@ -1,41 +1,20 @@
+import { sample } from '../../test'
 import { convertLocationSchemaToGeoJson } from './convertLocationSchemaToGeoJson'
-import { LocationWitnessPayload, LocationWitnessPayloadSchema } from './LocationWitnessPayload'
-
-const sample: LocationWitnessPayload = {
-  _archive: 'temp',
-  _client: 'js',
-  _hash: '319adee8ec88ccb4e8783e8b34776cf6afdd1b7ab6102b9e0f26a21920d84c09',
-  _id: '6217eaa075ecf7e73fa60352',
-  _observeDuration: 22,
-  _timestamp: 1645734560250,
-  currentLocation: {
-    coords: {
-      accuracy: 1983.9417522841113,
-      altitude: null,
-      altitudeAccuracy: null,
-      heading: null,
-      latitude: 30.1234567,
-      longitude: -85.1234567,
-      speed: null,
-    },
-    timestamp: 1645734561467,
-  },
-  schema: 'network.xyo.location',
-}
-
+import { LocationWitnessPayload } from './LocationWitnessPayload'
 describe('convertLocationSchemaToGeoJson', () => {
-  it('converts', () => {
-    const actual = convertLocationSchemaToGeoJson(sample)
-    expect(actual?.id).toBe(sample._hash)
+  it('converts data formatted according to the schema into a GeoJson Feature', () => {
+    const payload = sample as LocationWitnessPayload
+    const actual = convertLocationSchemaToGeoJson(payload)
+    expect(actual?.id).toBe(payload._hash)
     expect(actual?.type).toBe('Feature')
     expect(actual?.geometry).toBeTruthy()
     expect(actual?.geometry?.type).toBe('Point')
     expect(actual?.geometry?.coordinates).toBeTruthy()
     expect(Array.isArray(actual?.geometry?.coordinates)).toBeTruthy()
     expect(actual?.properties).toBeTruthy()
-    expect(actual?.properties?._archive).toBe(sample._archive)
-    expect(actual?.properties?._client).toBe(sample._client)
-    expect(actual?.properties?._timestamp).toBe(sample._timestamp)
-    expect(actual?.properties?.schema).toBe(sample.schema)
+    expect(actual?.properties?._archive).toBe(payload._archive)
+    expect(actual?.properties?._client).toBe(payload._client)
+    expect(actual?.properties?._timestamp).toBe(payload._timestamp)
+    expect(actual?.properties?.schema).toBe(payload.schema)
   })
 })
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,1 +1,2 @@
+export * from './sampleLocationWitness'
 export * from './testUtil.spec'
