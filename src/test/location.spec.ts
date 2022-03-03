e0bc27cfd0d52485fb5c205fe2a7e29ce196f017
JoelBCarter <joelbcarter@gmail.com>
2022-03-03 07:11:10 -0600

    DRY up tests

--- src/test/location.spec.ts
+++ src/test/location.spec.ts
@@ -1,6 +1,8 @@
 import {
   GetLocationQueryResponse,
+  LocationDivinerQueryCreationRequest,
   LocationDivinerQueryCreationResponse,
+  XyoArchivistApi,
   XyoPayload,
 } from '@xyo-network/sdk-xyo-client-js'
 import { FeatureCollection, Point } from 'geojson'
@@ -38,33 +40,42 @@ const validateGeoJsonFeatureCollection = (queryResult: FeatureCollection<Point, 
   expect(Array.isArray(queryResult?.features)).toBeTruthy()
 }
 
+const getQueryAnswer = async (
+  api: XyoArchivistApi,
+  queryCreationRequest: LocationDivinerQueryCreationRequest
+): Promise<FeatureCollection<Point, GeoJsonPointProperties>> => {
+  const queryCreationResponse = await createQuery(queryCreationRequest)
+  validateQueryCreationResponse(queryCreationResponse)
+  await delay(5000)
+  const queryAnswerResponse = await getQuery(queryCreationResponse.hash)
+  validateQueryAnswerResponse(queryAnswerResponse, queryCreationResponse)
+  const answerPayloads = await api.getBoundWitnessPayloadsByHash(queryAnswerResponse.answerHash || '')
+  validateQueryAnswerPayloads(answerPayloads)
+  const payload = answerPayloads.pop()
+  expect(payload).toBeTruthy()
+  expect(payload?.schema).toBe(answerSchema)
+  const answer = payload?.result as FeatureCollection<Point, GeoJsonPointProperties>
+  validateGeoJsonFeatureCollection(answer)
+  return answer
+}
+
 describe('Round trip tests', () => {
   const startTime = new Date().toISOString()
   const locationsToWitness = 5
   const api = getArchivist(testArchive)
   let stopTime = ''
   beforeAll(async () => {
+    await delay(1000)
     for (let location = 0; location < locationsToWitness; location++) {
       await witnessNewLocation(api)
-      await delay(1000)
     }
+    await delay(1000)
     stopTime = new Date().toISOString()
   })
   it('Generates answer if data was found', async () => {
     const queryCreationRequest = getValidRequest(testArchive, startTime, stopTime)
-    const queryCreationResponse = await createQuery(queryCreationRequest)
-    validateQueryCreationResponse(queryCreationResponse)
-    await delay(5000)
-    const queryAnswerResponse = await getQuery(queryCreationResponse.hash)
-    validateQueryAnswerResponse(queryAnswerResponse, queryCreationResponse)
-    const answerPayloads = await api.getBoundWitnessPayloadsByHash(queryAnswerResponse.answerHash || '')
-    validateQueryAnswerPayloads(answerPayloads)
-    const answer = answerPayloads.pop()
-    expect(answer).toBeTruthy()
-    expect(answer?.schema).toBe(answerSchema)
-    const result = answer?.result as FeatureCollection<Point, GeoJsonPointProperties>
-    validateGeoJsonFeatureCollection(result)
-    expect(result?.features?.length).toBe(locationsToWitness)
+    const answer = await getQueryAnswer(api, queryCreationRequest)
+    expect(answer?.features?.length).toBe(locationsToWitness)
   }, 10000)
   it('Generates an empty answer if no data was found', async () => {
     const now = new Date()
@@ -77,33 +88,10 @@ describe('Round trip tests', () => {
       futureStartTime.toISOString(),
       futureStopTime.toISOString()
     )
-    const queryCreationResponse: LocationDivinerQueryCreationResponse = await createQuery(queryCreationRequest)
-    validateQueryCreationResponse(queryCreationResponse)
-    await delay(5000)
-    const queryAnswerResponse = await getQuery(queryCreationResponse.hash)
-    validateQueryAnswerResponse(queryAnswerResponse, queryCreationResponse)
-    const answerPayloads = await api.getBoundWitnessPayloadsByHash(queryAnswerResponse.answerHash || '')
-    validateQueryAnswerPayloads(answerPayloads)
-    const answer = answerPayloads.pop()
-    expect(answer).toBeTruthy()
-    expect(answer?.schema).toBe(answerSchema)
-    const result = answer?.result as FeatureCollection<Point, GeoJsonPointProperties>
-    validateGeoJsonFeatureCollection(result)
-    expect(result?.features?.length).toBe(0)
+    const answer = await getQueryAnswer(api, queryCreationRequest)
+    expect(answer?.features?.length).toBe(0)
   }, 10000)
   it.skip('Handles bad/misshapen data', async () => {
-    // TODO: test
-  }, 10000)
-  it.skip('Handles missing source archivist', async () => {
-    // TODO: test
-  }, 10000)
-  it.skip('Handles missing source archive', async () => {
-    // TODO: test
-  }, 10000)
-  it.skip('Handles missing target archivist', async () => {
-    // TODO: test
-  }, 10000)
-  it.skip('Handles missing target archive', async () => {
     // TODO: test
   }, 10000)
 })
