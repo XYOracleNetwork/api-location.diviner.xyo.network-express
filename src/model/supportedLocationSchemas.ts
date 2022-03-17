69bf11d26d8bdb42516ff06060bd7a3cd0cc161d
JoelBCarter <joelbcarter@gmail.com>
2022-03-17 18:32:10 -0500

    Types for known location schemas

--- src/model/supportedLocationSchemas.ts
+++ src/model/supportedLocationSchemas.ts
@@ -1,6 +1,19 @@
-import { LocationWitnessPayload, LocationWitnessPayloadSchema } from '@xyo-network/sdk-xyo-client-js'
+import {
+  LocationWitnessPayload,
+  LocationWitnessPayloadSchema,
+  locationWitnessPayloadSchema,
+} from '@xyo-network/sdk-xyo-client-js'
 
-import { CurrentLocationWitnessPayload, CurrentLocationWitnessPayloadSchema } from './currentLocationWitness'
+import {
+  CurrentLocationWitnessPayload,
+  CurrentLocationWitnessPayloadSchema,
+  currentLocationWitnessPayloadSchema,
+} from './currentLocationWitness'
 
+export const supportedLocationWitnessSchemas = [locationWitnessPayloadSchema, currentLocationWitnessPayloadSchema]
+export const supportedLocationWitnessSchemasMap: Record<SupportedLocationWitnessPayloadSchemas, true> = {
+  'co.coinapp.currentlocationwitness': true,
+  'network.xyo.location': true,
+}
 export type SupportedLocationWitnessPayloadSchemas = LocationWitnessPayloadSchema | CurrentLocationWitnessPayloadSchema
 export type SupportedLocationWitnessPayloads = LocationWitnessPayload | CurrentLocationWitnessPayload
