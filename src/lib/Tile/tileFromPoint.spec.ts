6781d297374fa252383dd74df7e47e79f721d0df
JoelBCarter <joelbcarter@gmail.com>
2022-03-23 16:50:13 -0500

    Add tests for tileFromPoint

--- src/lib/Tile/tileFromPoint.spec.ts
+++ src/lib/Tile/tileFromPoint.spec.ts
@@ -10,10 +10,20 @@ interface PointZoomTile {
 }
 
 const testData: PointZoomTile[] = [
-  { point: point([-90, 180]), tile: [0, 0, 1], zoom: 1 },
-  { point: point([90, 180]), tile: [1, 0, 1], zoom: 1 },
-  { point: point([-90, -180]), tile: [0, 1, 1], zoom: 1 },
-  { point: point([90, -180]), tile: [1, 1, 1], zoom: 1 },
+  { point: point([-90, 45]), tile: [0, 0, 1], zoom: 1 },
+  { point: point([90, 45]), tile: [1, 0, 1], zoom: 1 },
+  { point: point([-90, -45]), tile: [0, 1, 1], zoom: 1 },
+  { point: point([90, -45]), tile: [1, 1, 1], zoom: 1 },
+
+  { point: point([-1, 1]), tile: [3, 3, 3], zoom: 3 },
+  { point: point([1, 1]), tile: [4, 3, 3], zoom: 3 },
+  { point: point([-1, -1]), tile: [3, 4, 3], zoom: 3 },
+  { point: point([1, -1]), tile: [4, 4, 3], zoom: 3 },
+
+  { point: point([-179, 89]), tile: [0, 0, 3], zoom: 3 },
+  { point: point([179, 89]), tile: [7, 0, 3], zoom: 3 },
+  { point: point([-179, -89]), tile: [0, 7, 3], zoom: 3 },
+  { point: point([179, -89]), tile: [7, 7, 3], zoom: 3 },
 ]
 
 describe('tileFromPoint', () => {
