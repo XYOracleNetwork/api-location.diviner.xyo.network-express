2ef0cd464f837663a5ea3e22d83dc92cf87cb501
JoelBCarter <joelbcarter@gmail.com>
2022-03-25 17:16:14 -0500

    Introduce snapshot tests

--- src/lib/Diviners/LocationQuadkeyHeatmapDiviner/getQuadkeyHeatmapFromPoints.spec.ts
+++ src/lib/Diviners/LocationQuadkeyHeatmapDiviner/getQuadkeyHeatmapFromPoints.spec.ts
@@ -1,33 +1,8 @@
-import { points, randomPoint } from '@turf/turf'
+import { points } from '@turf/turf'
 import { Position } from 'geojson'
 
 import { TestData, WithHashProperties, Zoom } from '../../../model'
-import { getQuadkeyHeatmapFromPoints, QuadkeyHeatmapTile } from './getQuadkeyHeatmapFromPoints'
-
-/**
- * Check if a number is close to another number, within the specified tolerance. Only here
- * because the Jest types don't support Jest's version of closeTo
- * https://jestjs.io/docs/expect#expectclosetonumber-numdigits
- * @param actual Actual number
- * @param expected Expected number
- * @param tolerance Allowed deviation (plus/minus) of the actual from the expected value
- * @returns True if within the specified tolerance, false otherwise
- */
-const closeTo = (actual: number, expected: number, tolerance: number): boolean => {
-  return Math.abs(actual - expected) < tolerance
-}
-
-const ensureResultIsValid = (actual: QuadkeyHeatmapTile[], expectedQuadkeys?: number) => {
-  expect(actual).toBeTruthy()
-  const tilesWithValue = actual.filter((h) => h.density !== 0).map((p) => p.density)
-  if (expectedQuadkeys) {
-    expect(tilesWithValue.length).toBe(expectedQuadkeys)
-  } else {
-    expect(tilesWithValue.length).toBeGreaterThan(0)
-  }
-  // const totalDensity = tilesWithValue.reduce((sum, a) => sum + a, 0)
-  // expect(closeTo(totalDensity, 100, 10)).toBeTruthy()
-}
+import { getQuadkeyHeatmapFromPoints } from './getQuadkeyHeatmapFromPoints'
 
 type TestDataInput = {
   coordinates: Position[]
@@ -57,12 +32,7 @@ const testData: TestData<TestDataInput, TestDataExpected>[] = [
 describe('getQuadkeyHeatmapFromPoints', () => {
   it.each(testData)('calculates with known points', (data) => {
     const { coordinates, zoom } = data.input
-    const { quadkeys } = data.expected
-    const locations = points<WithHashProperties>(coordinates, { hash: 'foo' })
-    ensureResultIsValid(getQuadkeyHeatmapFromPoints(locations, zoom), quadkeys)
-  })
-  it('calculates with random points', () => {
-    const locations = randomPoint(1000, { bbox: [-179, -85, 179, 85] })
-    ensureResultIsValid(getQuadkeyHeatmapFromPoints(locations, 1))
+    const locations = points<WithHashProperties>(coordinates, { hash: '' })
+    expect(getQuadkeyHeatmapFromPoints(locations, zoom)).toMatchSnapshot()
   })
 })
