88fa246864ae4bede1de49f965eed4987afbacae
JoelBCarter <joelbcarter@gmail.com>
2022-03-31 17:26:40 -0500

    Update test data format

--- src/lib/Quadkey/getQuadkeysByParent.spec.ts
+++ src/lib/Quadkey/getQuadkeysByParent.spec.ts
@@ -1,15 +1,16 @@
 import { getQuadkeysByParent } from './getQuadkeysByParent'
 
-const testData: string[][] = [
-  ['00', '033', '0333'],
-  ['10', '122', '1222'],
-  ['20', '211', '2111'],
-  ['30', '300', '3000'],
+const testData = [
+  { input: ['00', '033', '0333'] },
+  { input: ['10', '122', '1222'] },
+  { input: ['20', '211', '2111'] },
+  { input: ['30', '300', '3000'] },
+  { input: ['40', '40', '40'] },
 ]
 
 describe('getQuadkeysByParent', () => {
   it.each(testData)('converts the quadkey at the specified zoom level', (data) => {
-    const actual = getQuadkeysByParent([...data])
+    const actual = getQuadkeysByParent(data.input)
     expect(actual).toMatchSnapshot()
   })
 })
