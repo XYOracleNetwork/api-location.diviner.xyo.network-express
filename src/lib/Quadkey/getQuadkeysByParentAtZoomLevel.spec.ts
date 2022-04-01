de2b33bca17900e54875d678c03ba65f4b07621b
JoelBCarter <joelbcarter@gmail.com>
2022-03-31 19:41:02 -0500

    Update tests

--- src/lib/Quadkey/getQuadkeysByParentAtZoomLevel.spec.ts
+++ src/lib/Quadkey/getQuadkeysByParentAtZoomLevel.spec.ts
@@ -1,17 +1,18 @@
+import { Zoom } from '../../model'
 import { getQuadkeysByParentAtZoomLevel } from './getQuadkeysByParentAtZoomLevel'
 
 const testData = [
-  { input: { quadkeys: ['00', '033', '0333'], zoom: 1 } },
-  { input: { quadkeys: ['100', '1220', '12220'], zoom: 2 } },
-  { input: { quadkeys: ['2000', '21100', '211100'], zoom: 3 } },
-  { input: { quadkeys: ['30000', '300000', '3000000'], zoom: 4 } },
-  { input: { quadkeys: ['400000', '400000', '400000'], zoom: 5 } },
+  { input: { quadkeys: ['000', '0330', '03330'], zoom: 2 } },
+  { input: { quadkeys: ['1000', '12200', '122200'], zoom: 3 } },
+  { input: { quadkeys: ['20000', '211000', '2111000'], zoom: 4 } },
+  { input: { quadkeys: ['300000', '3000000', '30000000'], zoom: 5 } },
+  { input: { quadkeys: ['4000000', '4000000', '4000000'], zoom: 6 } },
 ]
 
 describe('getQuadkeysByParent', () => {
   it.each(testData)('converts the quadkey at the specified zoom level', (data) => {
     const { quadkeys, zoom } = data.input
-    const actual = getQuadkeysByParentAtZoomLevel(quadkeys, zoom)
+    const actual = getQuadkeysByParentAtZoomLevel(quadkeys, zoom as Zoom)
     expect(actual).toMatchSnapshot()
   })
 })
