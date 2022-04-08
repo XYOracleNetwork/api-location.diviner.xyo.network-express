1ef21d2f06f3c5c274fe1f960cf07ff36ef8d4c9
Joel Carter <JoelBCarter@users.noreply.github.com>
2022-04-08 15:13:12 -0500

    Remove Heatmap Density Normalization (#51)

--- src/lib/Diviners/LocationQuadkeyHeatmapDiviner/getQuadkeyHeatmapFromPoints.ts
+++ src/lib/Diviners/LocationQuadkeyHeatmapDiviner/getQuadkeyHeatmapFromPoints.ts
@@ -6,7 +6,6 @@ import {
   getQuadkeyAtZoomLevel,
   getQuadkeysByParent,
   getQuadkeysByParentAtZoomLevel,
-  getZoomLevel,
 } from '../../Quadkey'
 
 /**
@@ -19,13 +18,13 @@ const minLocationsPerQuadkey = 2
  * The minimum zoom to use when calculating quadkeys for the
  * heatmap
  */
-const minHeatmapZoom: Zoom = 9
+const minHeatmapZoom: Zoom = 11
 
 /**
  * The maximum zoom to use when calculating quadkeys for the
  * heatmap
  */
-const maxHeatmapZoom: Zoom = 12
+const maxHeatmapZoom: Zoom = 11
 
 /**
  * Multiplier to ensure that density is higher for the same number of
@@ -34,7 +33,7 @@ const maxHeatmapZoom: Zoom = 12
  * using zoom as the "area" since a higher zoom directly corresponds
  * to a smaller area and vice versa
  */
-const densityZoomMultiplier = 1 / (minLocationsPerQuadkey * maxHeatmapZoom)
+// const densityZoomMultiplier = 1 / (maxHeatmapZoom - 1)
 
 /**
  * Recursively decompose an array of quadkeys into a heatmap based on
@@ -72,23 +71,56 @@ export const getQuadkeyHeatmapFromPoints = (
   const heatmap = rollup(quadkeys, minHeatmapZoom)
   const quadkeysByParent = getQuadkeysByParent(heatmap)
   const quadkeysWithDensity = Object.keys(quadkeysByParent).map<QuadkeyWithDensity>((parent) => {
-    const parentZoom = getZoomLevel(parent)
+    // const parentZoom = getZoomLevel(parent)
     return {
       // Normalize density to zoom level so that same number of points
       // at higher zoom is a higher density
-      density: quadkeysByParent[parent].length * parentZoom * densityZoomMultiplier,
+      // density: quadkeysByParent[parent].length * parentZoom * densityZoomMultiplier,
+      density: quadkeysByParent[parent].length,
       quadkey: parent,
     }
   })
   // Find the maximum density across all results
-  const maxDensity = Math.max(...quadkeysWithDensity.map((q) => q.density))
+  // const densities = quadkeysWithDensity.map((q) => q.density)
+  // const minDensity = Math.min(...densities)
+  // const maxDensity = Math.max(...densities)
+  // const [, median, standardDeviation] = calculateDistribution(densities)
+  // const roundedMedian = Math.max(Math.round(median), 2)
+  // const roundedStandardDeviation = Math.round(standardDeviation)
+  // const lowerBound = Math.round(Math.max(roundedMedian - roundedStandardDeviation, 1))
+  // const upperBound = Math.max(Math.abs(Math.round(roundedMedian - lowerBound)) + roundedMedian, 3)
 
-  // Using the max density, shift the range of all the quadkey densities to
-  // ensure that the resultant density is always always in the range [0.0, 1.0]
-  return quadkeysWithDensity.map((q) => {
-    return {
-      density: q.density / maxDensity,
-      quadkey: q.quadkey,
-    }
-  })
+  // Scale the range of all the quadkey densities to ensure that the
+  // resultant density is always always in the range [0.0, 1.0]
+  return (
+    quadkeysWithDensity
+      // .map((q) => {
+      //   const clampedDensity = clamp(q.density, lowerBound, upperBound)
+      //   const scaledDensity = range(lowerBound, upperBound, 0.1, 0.9, clampedDensity)
+      //   return {
+      //     density: scaledDensity,
+      //     quadkey: q.quadkey,
+      //   }
+      // })
+      .sort((a, b) => b.density - a.density)
+  )
 }
+
+// const calculateDistribution = (densities: number[]): [number, number, number] => {
+//   const count = densities.length
+//   if (!count) {
+//     return [0, 0, 0]
+//   }
+//   const sum = densities.reduce((acc, n) => acc + n, 0)
+//   const mean = sum / count
+//   const variance = densities.reduce((acc, n) => (acc += (n - mean) * (n - mean)), 0) / count
+//   const standardDeviation = Math.sqrt(variance)
+//   const sortedDensities = densities.sort((a, b) => a - b)
+//   const median = sortedDensities[Math.floor(count / 2)]
+//   return [mean, median, standardDeviation]
+// }
+
+// const lerp = (x: number, y: number, a: number) => x * (1 - a) + y * a
+// const invlerp = (x: number, y: number, a: number) => clamp((a - x) / (y - x))
+// const clamp = (a: number, min = 0, max = 1) => Math.min(max, Math.max(min, a))
+// const range = (x1: number, y1: number, x2: number, y2: number, a: number) => lerp(x2, y2, invlerp(x1, y1, a))
